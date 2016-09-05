1c01d03278e980a0fe08d3cf1c77238722b5999b
Roberto <Roberto@ST4CK-WINDOWS10>
2016-09-05 14:02:18 +0200

    Revert "Fixing again pokedex visual bug and checking for null reference"
    This reverts commit 80275b703722f22508598c7af29f1052fd2e47ee.

--- PokemonGo-UWP/ViewModels/PokedexPageViewModel.cs
+++ PokemonGo-UWP/ViewModels/PokedexPageViewModel.cs
@@ -124,9 +124,6 @@ namespace PokemonGo_UWP.ViewModels
             (_openPokedexEntry = new DelegateCommand<KeyValuePair<PokemonId, PokedexEntry>>(
                 (x) =>
                 {
-                    // Check if Pokemon was never seen
-                    if (x.Value == null) return;
-
                     NavigationService.Navigate(typeof(PokedexDetailPage), x.Key);
                 },
                     (x) => true)
